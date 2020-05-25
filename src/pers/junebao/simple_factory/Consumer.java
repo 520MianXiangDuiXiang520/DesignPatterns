ecfc0c9d49afafea5ceeed34dfd34c0d01ff755e
520MianXiangDuiXiang520 <15364968962@163.com>
2020-05-25 21:55:38 +0800

    简单工厂

--- src/pers/junebao/simple_factory/Consumer.java
+++ src/pers/junebao/simple_factory/Consumer.java
@@ -2,9 +2,6 @@ package pers.junebao.simple_factory;
 
 import pers.junebao.simple_factory.phone.BasePhone;
 
-/**
- * @author lenovo
- */
 public class Consumer {
     public static void main(String[] args) {
         BasePhone phone = PhoneFactory.getPhone("Honor");
